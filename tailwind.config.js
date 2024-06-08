14ea7bd609bcb1d082b4f8ff653b798609995f1b
hrutujamirgal <hrutuja.mirgal21@vit.edu>
2024-06-08 23:14:39 +0530

    main commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+
+module.exports = {
+    theme: {
+      extend: {
+        colors: {
+          'black-500': '#1e1e1e',
+          'black-700': '#0d0d0d',
+        },
+      },
+    },
+    variants: {},
+    plugins: [],
+  }
+  
